c377567f517a4b84de77657cb10517a712c30eb7
Miguel Torres <mgltorsa@gmail.com>
2022-10-18 14:56:34 -0400

    updated experiment script

--- tiling/experiments/speed-up/experiment.sh
+++ tiling/experiments/speed-up/experiment.sh
@@ -10,25 +10,16 @@
 #SBATCH --open-mode=append
 
 
-#Matrix mult
 for i in {1..32}
 do
+    #Matrix mult
     srun --nodes=1 --ntasks=1 --cpus-per-task=$i --exclusive matrix-multiplication/ParallelNonTiled "$i" 2000 &
     srun --nodes=1 --ntasks=1 --cpus-per-task=$i --exclusive matrix-multiplication/ParallelTiled "$i" 2000 &
-    wait
-done
-
-#Vector mult
-for i in {1..32}
-do
+    #vector
     srun --nodes=1 --ntasks=1 --cpus-per-task=$i --exclusive vector-multiplication/ParallelNonTiled "$i" 2000 &
     srun --nodes=1 --ntasks=1 --cpus-per-task=$i --exclusive vector-multiplication/ParallelTiled "$i" 2000 &
-    wait
-done
 
-#Jacobi
-for i in {1..32}
-do
+    #jacobi
     srun --nodes=1 --ntasks=1 --cpus-per-task=$i --exclusive jacobi/ParallelNonTiled "$i" 2000 &
     srun --nodes=1 --ntasks=1 --cpus-per-task=$i --exclusive jacobi/ParallelTiled "$i" 2000 &
     wait
