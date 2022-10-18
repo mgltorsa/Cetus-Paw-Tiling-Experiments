987580463b08d2fc1e0f47da691922575493b82e
Miguel Torres <mgltorsa@gmail.com>
2022-10-18 14:48:50 -0400

    added compile script

---
+++ tiling/experiments/speed-up/compile.sh
@@ -0,0 +1,12 @@
+
+#Matrix
+gcc -g -fopenmp matrix-multiplication/ParallelNonTiled.c -o matrix-multiplication/ParallelNonTiled
+gcc -g -fopenmp matrix-multiplication/ParallelTiled.c -o matrix-multiplication/ParallelTiled
+
+#Vector
+gcc -g -fopenmp vector-multiplication/ParallelNonTiled.c -o vector-multiplication/ParallelNonTiled
+gcc -g -fopenmp vector-multiplication/ParallelTiled.c -o vector-multiplication/ParallelTiled
+
+#Jacobi
+gcc -g -fopenmp jacobi/ParallelNonTiled.c -o jacobi/ParallelNonTiled
+gcc -g -fopenmp jacobi/ParallelNonTiled.c -o jacobi/ParallelNonTiled
--- tiling/experiments/speed-up/experiment.sh
+++ tiling/experiments/speed-up/experiment.sh
@@ -11,10 +11,6 @@
 
 
 #Matrix mult
-
-gcc -g -fopenmp matrix-multiplication/ParallelNonTiled.c -o matrix-multiplication/ParallelNonTiled
-gcc -g -fopenmp matrix-multiplication/ParallelTiled.c -o matrix-multiplication/ParallelTiled
-
 for i in {1..32}
 do
     srun --nodes=1 --ntasks=1 --cpus-per-task=$i --exclusive matrix-multiplication/ParallelNonTiled "$i" 2000 &
@@ -23,9 +19,6 @@ do
 done
 
 #Vector mult
-gcc -g -fopenmp vector-multiplication/ParallelNonTiled.c -o vector-multiplication/ParallelNonTiled
-gcc -g -fopenmp vector-multiplication/ParallelTiled.c -o vector-multiplication/ParallelTiled
-
 for i in {1..32}
 do
     srun --nodes=1 --ntasks=1 --cpus-per-task=$i --exclusive vector-multiplication/ParallelNonTiled "$i" 2000 &
@@ -34,10 +27,6 @@ do
 done
 
 #Jacobi
-gcc -g -fopenmp jacobi/ParallelNonTiled.c -o jacobi/ParallelNonTiled
-gcc -g -fopenmp jacobi/ParallelNonTiled.c -o jacobi/ParallelNonTiled
-
-
 for i in {1..32}
 do
     srun --nodes=1 --ntasks=1 --cpus-per-task=$i --exclusive jacobi/ParallelNonTiled "$i" 2000 &
